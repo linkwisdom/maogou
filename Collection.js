2b2a9413dcd1a72a1bf7419c406f0f97320b4f15
linkwisdom <liu@liandong.org>
2013-08-02 14:29:07 +0800

    update comments

--- Collection.js
+++ Collection.js
@@ -1,5 +1,10 @@
 var events = require('events');
 
+/**
+ * mapList 对应函数的扩展链式函数的映射对象
+ * 
+ * 如 db.find({level: 3}).limit(1).sort('age').max(30)
+ */
 var mapList = {
     find: ['fields', 'limit', 'sort', 'skip', 'min', 'max'],
     findOne: ['fields', 'limit', 'sort', 'skip', 'min', 'max'],
@@ -7,6 +12,12 @@ var mapList = {
     geoNear: ['num', 'maxDistance', 'query', 'uniqueDocs']
 };
 
+/**
+ * 增强组件，实现mongodb风格，链式调用，promise范式
+ *  
+ * @param {String} fname 函数名称
+ * @param {Object} options 扩展参数(引用对象)
+ */
 function Augment(fname, options) {
     var me = this;
     var emitter = new events.EventEmitter();
@@ -14,6 +25,7 @@ function Augment(fname, options) {
     var funs = mapList[fname];
     var taskQue = [];
 
+    //将链式函数传值作为参数映射到扩展参数集
     function parameterize(item) {
         me[item] = function(value) {
             options[item] = value;
@@ -21,6 +33,7 @@ function Augment(fname, options) {
         };
     }
     
+    //执行promise任务链
     this.resolve = function(err, result) {
         if (err) {
             err.func = fname;
@@ -37,17 +50,21 @@ function Augment(fname, options) {
             
         } while(fun);
         
+        //任务执行完毕后，发送成功事件
         emitter.emit('ok', result);
     };
     
+    //执行失败，发送fail事件
     this.onError = function(callback) {
         emitter.on('error', callback);
     };
+    
     
     this.onSuccess = function(callback) {
         emitter.on('ok', callback);
     };
 
+    //直接增加扩展属性集
     this.set = function(params) {
         for (var item in params) {
             options[item] = params[item];
@@ -55,6 +72,7 @@ function Augment(fname, options) {
         return me;
     };
 
+    //增加promise任务链
     this.then = function(callback) {
         taskQue.push(callback);
         return this;
@@ -71,6 +89,13 @@ function Augment(fname, options) {
     }
 }
 
+/**
+ * Collection对象
+ * 封装mongodb集合对象的方法，通过Augment扩展
+ *  
+ * @param {Object} name
+ * @param {Object} database
+ */
 function Collection(name, database) {
     var db = database;
     var me = this;
